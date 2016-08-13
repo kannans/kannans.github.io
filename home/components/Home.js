c7e76260996cfaac1c1622ed4d0c6daecd19ed2c
kannans <ksevugaperumal@sycamoreinformatics.com>
2016-08-13 21:25:15 +0530

    Home page with boostrap

--- home/components/Home.js
+++ home/components/Home.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import Modal from './Modal'
+import { Button, Jumbotron, Grid, Row, Col } from 'react-bootstrap'
 
 export default React.createClass({
   render() {
-    return <div>Hello, Welcome to my page kannansv.in</div>
+    return (
+      <div>
+        <Grid>
+          <Row className="show-grid">
+            <Col xs={12} md={12}>
+              <Jumbotron>
+                <h1>Hello, world!</h1>
+                <p>This is a simple React app with Ruby on Rails api calls.</p>
+                <Modal />
+              </Jumbotron>
+            </Col>
+          </Row>
+        </Grid>
+
+        <footer className="footer">
+          <div className="container">
+            <p className="text-muted">Place sticky footer content here.</p>
+          </div>
+        </footer>
+      </div>
+    )
   }
 })
