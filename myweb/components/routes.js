da7f13968acdf8e98c5332d0c67eeed95a8c1cc2
kannans <ksevugaperumal@sycamoreinformatics.com>
2016-08-14 21:29:51 +0530

    React Router

--- myweb/components/routes.js
+++ myweb/components/routes.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { Route, IndexRoute } from 'react-router'
+import {Router, browserHistory, Route, IndexRoute } from 'react-router'
 import App from './App'
 import About from './About'
 import Home from './Home'
@@ -8,10 +8,12 @@ import Contact from './Contact'
 import Works from './Works'
 
 module.exports = (
-  <Route path="/" component={App}>
-    <IndexRoute component={Home}/>
-    <Route path="/about" component={About}/>
-    <Route path="/contact" component={Contact}/>
-    <Route path="/works" component={Works}/>
-  </Route>
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
+      <Route path="/works" component={Works}/>
+    </Route>
+  </Router>
 );
