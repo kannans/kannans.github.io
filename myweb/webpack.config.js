80f36b54db23d1a4387372f8cf3a038755825cc5
kannans <ksevugaperumal@sycamoreinformatics.com>
2016-08-15 13:57:17 +0530

    Node module

--- myweb/webpack.config.js
+++ myweb/webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack')
+
 module.exports = {
   entry: './index.js',
 
@@ -6,6 +8,13 @@ module.exports = {
     publicPath: ''
   },
 
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
